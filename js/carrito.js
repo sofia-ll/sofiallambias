6d8120c5de18123b4c8ee5a2a026094bf594ef93
SOFIA LLAMBIAS <116291862+sofia-ll@users.noreply.github.com>
2023-04-26 09:50:19 -0300

    commit diecinueve

---
+++ js/carrito.js
@@ -0,0 +1,123 @@
+
+const tbody = document.querySelector("tbody")
+
+function retornoLaminaCarrito(lamina) {
+                        return `<tr>
+                            <th scope="row">
+                            <img class="img_obras_carrito" src="${lamina.imagen}" alt="${lamina.alt}">
+                            </th>
+                            
+                            <td class="carrito-laminas-titulo">
+                                <small class="negrita">Título</small>
+                                <p>${lamina.nombre}</p>
+                            </td>
+                            <td class="carrito-lamina-tamano">
+                                <small>Tamaño</small>
+                                <p>${lamina.tamano}</p>
+                            </td>
+                            <td class="carrito-lamina-precio">
+                                <small>Precio</small>
+                                <p>$${lamina.precio}</p>
+                            </td>
+                            <td class="carrito-lamina-unidades">
+                                <small>Unidades</small>
+                                <p>${lamina.unidades} </p>
+                            </td>
+
+                            <td>
+                                <button id="${lamina.numeroLamina}" class="carrito-laminas-agregar"> 
+                                <i class="bi bi-bag-plus-fill"></i>
+                                </button>
+                            </td>
+
+                            <td>
+                                <button id="${lamina.numeroLamina}" class="carrito-laminas-eliminar"> 
+                                <i class="bi bi-trash-fill"></i>
+                                </button>
+                            </td>
+
+                        </tr>`
+}
+
+// imagen:"../img/litografia/paradigmas1.jpg", numeroLamina: 0, nombre: "Litografía 1", precio: 10000, tamano: "35 cm x 50 cm", id: "cero"},
+
+function cargarLaminas(array){
+array.forEach(element => {
+    tbody.innerHTML += retornoLaminaCarrito(element)
+})
+onClickBotones();
+}
+
+cargarLaminas(laminas)
+
+function onClickBotones(){
+    const buttons = document.querySelectorAll("button")
+    for (boton of buttons){
+        boton.addEventListener("click",(e)=> {
+        agregarAlCarrito(e.target.numeroLamina)
+        })
+    }
+}
+
+function agregarAlCarrito(numeroLamina) {
+	let resultado = laminas.find((lamina) => lamina.numeroLamina === parseInt(numeroLamina));
+	if (resultado !== undefined) {
+		carrito.push(resultado);
+		console.log("Se agregó la lámina", resultado.nombre, "al carrito.");
+		guardarCarrito(carrito);
+	}
+}
+
+function guardarCarrito(carrito){
+    if (carrito.length > 0){
+        localStorage.setItem("carritoLaminas", JSON.stringitfy(carrito))
+    }
+}
+
+function recuperarCarrito(){
+    const recuperoCarrito = JSON.parse(localStorage.getItem("carritoLaminas")) 
+    console.table()
+
+}
+
+function terminarCompra(){
+        if(carrito.length === 0){
+            console.warn("¡Tu carrito está vacío! 😞")
+            return
+        }
+        else {
+            subtotal()
+        }
+            }
+
+function subtotal(){
+    const total = carrito.reduce((acc, lamina) => acc + lamina.precio * unidades, 0).toFixed(2)
+    confirm(`El total es de $ ${total}`)
+    console.log(total)
+}
+
+
+let unidades = parseInt(prompt("¿Cuántas unidades querés llevar?"))
+
+function iniciarCompra() {
+
+    let numeroLamina = prompt("¿Cuál es el número de la lámina que querés comprar?")
+    let laminaComprada = buscarLamina(numeroLamina)
+        
+    if (laminaComprada === undefined) {
+        alert("⛔️ Elegí una lámina, por favor.")
+        return
+    }
+    else {
+        carrito.push(laminaComprada)
+        alert(`La lámina ${laminaComprada.nombre} se agregó tu carrito. 😀`)
+        let unidades = parseInt(prompt("¿Cuántas unidades querés llevar?"))
+        let seguir = confirm("¿Querés elegir otra lámina?")
+        if(seguir) {
+            iniciarCompra()
+        }
+        else{
+            terminarCompra()
+        }
+    }
+    }
--- js/main.js
+++ js/main.js
@@ -10,12 +10,12 @@ const mensajeCuatro = "Te invito a que recorras mi sitio web. Vas a ver que hago
 
 const carrito = []
 
-const laminas = [{imagen:"../img/litografia/paradigmas1.jpg", numeroLamina: 0, nombre: "Litografía 1", precio: 10000, tamano: "35 cm x 50 cm", id: "cero"},
-                {imagen:"../img/escultura/esc1.jpg", numeroLamina: 1, nombre: "Escultura 1", precio: 10000, tamano: "35 cm x 50 cm", id: "uno" },
-                {imagen:"../img/pintura1.jpg", numeroLamina: 2, nombre: "Pintura 1", precio: 12000, tamano: "50 cm x 50 cm" id: "dos" },
-                {imagen:"../img/calados1.jpg", numeroLamina: 3, nombre: "Calado 1", precio: 10000, tamano: "35 cm x 50 cm", id: "tres"  },
-                {imagen:"../img/dibujo1.jpg", numeroLamina: 4, nombre: "Dibujo 1", precio: 12000, tamano: "50 cm x 50 cm", id: "cuatro" },
-                {imagen:"../img/fotografia1.jpg", numeroLamina: 5, nombre: "Fotografía 1", precio: 10000, tamano: "35 cm x 50 cm", id: "cinco" }];
+const laminas = [{imagen:"../img/litografia/paradigmas1.jpg", alt:"obras tecnica litografia", numeroLamina: 0, nombre: "Litografía 1", precio: 10000, tamano: "35 cm x 50 cm"},
+                {imagen:"../img/escultura/escultura1.jpg", alt:"obras tecnica escultura", numeroLamina: 1, nombre: "Escultura 1", precio: 10000, tamano: "35 cm x 50 cm"},
+                {imagen:"../img/pintura1.jpg", alt:"obras tecnica pintura", numeroLamina: 2, nombre: "Pintura 1", precio: 12000, tamano: "50 cm x 50 cm"},
+                {imagen:"../img/calados1.jpg", alt:"obras tecnica calado", numeroLamina: 3, nombre: "Calado 1", precio: 10000, tamano: "35 cm x 50 cm"},
+                {imagen:"../img/dibujo1.jpg", alt:"obras tecnica dibujo", numeroLamina: 4, nombre: "Dibujo 1", precio: 12000, tamano: "50 cm x 50 cm"},
+                {imagen:"../img/fotografia1.jpg", alt:"obras tecnica fotografia", numeroLamina: 5, nombre: "Fotografía 1", precio: 10000, tamano: "35 cm x 50 cm"}];
 
 function comprarLaminas(){
     
@@ -36,116 +36,64 @@ function comprarLaminas(){
         }
 }
 
-let unidades = parseInt(prompt("¿Cuántas unidades querés llevar?"))
-
-function iniciarCompra() {
-
-    let numeroLamina = prompt("¿Cuál es el número de la lámina que querés comprar?")
-    let laminaComprada = buscarLamina(numeroLamina)
-        
-    if (laminaComprada === undefined) {
-        alert("⛔️ Elegí una lámina, por favor.")
-        return
-    }
-    else {
-        carrito.push(laminaComprada)
-        alert(`La lámina ${laminaComprada.nombre} se agregó tu carrito. 😀`)
-        let unidades = parseInt(prompt("¿Cuántas unidades querés llevar?"))
-        let seguir = confirm("¿Querés elegir otra lámina?")
-        if(seguir) {
-            iniciarCompra()
-        }
-        else{
-            terminarCompra()
-        }
-    }
-    }
-
 function buscarLamina(numeroLamina) {
     let busquedaLamina = laminas.find(lamina => lamina.numeroLamina === parseInt(numeroLamina))
         return busquedaLamina;
 }
 
-function terminarCompra(){
-        if(carrito.length === 0){
-            console.warn("¡Tu carrito está vacío! 😞")
-            return
-        }
-        else {
-            subtotal()
-        }
-            }
-
-function subtotal(){
-    const total = carrito.reduce((acc, lamina) => acc + lamina.precio * unidades, 0).toFixed(2)
-    confirm(`El total es de $ ${total}`)
-    console.log(total)
-}
-
-// function cardsDinamicasMobile(){
-    const cardsDinamicasMob = (lamina)=>{
-        return `<div class="col fotoperfilcv mainmobile">
-            <div class="card" style="width: 100%;">
-                    <div class="card-img-top">
-                        ${lamina.imagen}   
-                    </div>
-                    <div class="card-body text-center">
-                        <p class="card-text fs-6 justify producto"><span class="bold">0 - </span> ${lamina.nombre} </p>
-                    </div>
-                    <div class="card-body text-center">
-                        <p class="card-text fs-6 justify tamaño"><span class="bold">${lamina.tamano}</span> </p>
-                    </div>
-                    <div class="card-body text-center">
-                        <p class="card-text fs-6 justify precio"><span class="bold">${lamina.precio}</span> </p>
-                    </div>
-                    <div class="card-body text-center">
-                        <p class="card-text fs-6 justify precio"><span class="bold">${lamina.unidades}</span> </p>
-                    </div>
-                    <div class="card-body text-center boton-cards">
-                        <button class="card-text fs-6 justify button ${lamina.id} ">AGREGAR</button>
-                    </div>
-            </div>
-        </div>`
-    }
-// }
-
-const tbody = document.querySelector("tbody")
-
-function retornoLaminaCarrito(lamina) {
-                        return `<tr>
-                            <th scope="row">
-                                ${lamina.imagen}
-                            </th>
-                            <td class="carrito-laminas">
-                                <small>Título</small>
-                                <h3>${lamina.nombre}</h3>
-                            </td>
-                            <td class="carrito-lamina-tamano">
-                                <small>Tamaño</small>
-                                <p>${lamina.tamano}</p>
-                            </td>
-                            <td class="carrito-lamina-precio">
-                                <small>Precio</small>
-                                <p>${lamina.precio}$</p>
-                            </td>
-                            <td class="carrito-lamina-unidades">
-                                <small>Unidades</small>
-                                <p>$ ${lamina.unidades} </p>
-                            </td>
-
-                            <td>
-                                <button class="carrito-producto-eliminar"> 
-                                <i class="bi bi-trash-fill"></i>
+function cardsDinamicasHTML(lamina){
+        return `<article class="artimgobras">
+                    <img class="img_obras" src="${lamina.imagen}" alt="${lamina.alt}">
+                    <div class="cont">
+                        <div class="card-body text-center">
+                            <p class="card-text fs-6 justify producto titulo"><span class="bold"></span> ${lamina.nombre}</p>
+                        </div>
+                        <div class="card-body text-center">
+                            <p class="card-text fs-6 justify tamano"><span class="bold">${lamina.tamano}</span> </p>
+                        </div>
+                        <div class="card-body text-center">
+                            <p class="card-text fs-6 justify precio"><span class="bold">$ ${lamina.precio}</span> </p>
+                        </div>
+                        <div class="card-body text-center">
+                            <button class="card-text fs-6 justify button boton_tienda" id="${lamina.numeroLamina}">
+                                Agregar
                             </button>
-                            </td>
-                        </tr>`
+                        </div>
+                    </div>
+            </article>`
+    }
+
+function onClickBotones() {
+	const buttons = document.querySelectorAll("button");
+    if (botones !== null){
+        for (const button of buttons) {
+		button.addEventListener("click", (e) => {
+			agregarAlCarrito(e.target.id);
+		});
+	}
+    }
+	
 }
 
+function cargarLaminas(array) {
+    imagenes_obras.innerHTML = ""
+    array.forEach((lamina) => { imagenes_obras.innerHTML += cardsDinamicasHTML(lamina) })
+    onClickBotones()
+}
 
-// imagen:"../img/litografia/paradigmas1.jpg", numeroLamina: 0, nombre: "Litografía 1", precio: 10000, tamano: "35 cm x 50 cm", id: "cero"},
+onClickBotones()
 
-function cargarLaminas(array){
-array.array.forEach(element => {
-    tbody.innerHTML += retornoLaminaCarrito(element)
-});
+function agregarAlCarrito(numeroLamina) {
+	let resultado = laminas.find((lamina) => lamina.numeroLamina === parseInt(numeroLamina));
+	if (resultado !== undefined) {
+		carrito.push(resultado);
+		console.log("Se agregó la lámina", resultado.nombre, "al carrito.");
+		guardarCarrito(carrito);
+	}
+}
+
+function guardarCarrito(carrito) {
+	if (carrito.length > 0) {
+		localStorage.setItem("carritoLaminas", JSON.stringify(carrito));
+	}
 }
